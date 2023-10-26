b7580676bf0b79f1641905d195ec7f3c654a5553
Miran <mirankavinda12@gmail.com>
2023-10-26 17:47:28 +0530

    loop_keypress

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 import curses
 from curses import wrapper
 
+def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.addstr("Welcome to Speed Typing Test!")
+    stdscr.addstr("\nPress any key to start!")
+    stdscr.refresh()
+    stdscr.getkey()
+
+
+def wpm_test(stdscr):
+    target_text = "Hello Worls this is some test text for this app!"
+    current_text = []
+
+    stdscr.clear()
+    stdscr.addstr(target_text)
+    stdscr.refresh()
+
+    while True:
+        key = stdscr.getkey()
+        current_text.append(key)
+
+        for char in current_text:
+            stdscr.addstr(char, curses.color_pair(1))
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
-
-    stdscr.clear()
-    stdscr.addstr(0, 0, "Hello World")
-    stdscr.refresh()
-    key = stdscr.getkey()
-    print(key)
+ 
+    start_screen(stdscr)
+    wpm_test(stdscr)
 
 wrapper(main)
